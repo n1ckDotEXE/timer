26f29e7c36b9a5ea775b608681cd2fa8ca184205
n1ckDotEXE <admin@nicholasnam.com>
2020-11-09 15:15:09 -0500

    Version 2 complete

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 // version 0:
 // console.log('\u0007');
+
+// Verison 1 ////////////////////////
+// setTimeout(function() {
+//     console.log('3 seconds passed')
+// }, 3000)
+
+// Version 2 ////////////////////////
+var input = process.argv[2];
+setTimeout(function() {
+    console.log(`${input} second(s) have passed`)
+}, input*1000)
+
+// Version 3 ////////////////////////
